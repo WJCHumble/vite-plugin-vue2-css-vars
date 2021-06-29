94cdca98f58702f88fc5d9cfb51840ca110d75c2
wujingchang <1957503379@qq.com>
2021-06-29 10:47:00 +0800

    fix: add postcss-scss to support parse `//` comment in css

--- src/style.ts
+++ src/style.ts
@@ -1,14 +1,14 @@
 
-import { RawSourceMap } from '@vue/component-compiler-utils/dist/types';
 import postcss from 'postcss'
 import { TransformResult } from 'vite';
 import { cssVarsPlugin } from "./cssVars"
+import postScss from "postcss-scss"
 
 export async function transformStyle(
   code: string, 
   id: string
 ): Promise<TransformResult> {
-  const { css } = await postcss([cssVarsPlugin({ id })]).process(code, {from: undefined});
+  const { css } = await postcss([cssVarsPlugin({ id })]).process(code, {from: undefined, parser: postScss});
   return {
     code: css,
     map: null
